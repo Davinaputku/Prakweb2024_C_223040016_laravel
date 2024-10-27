bbea684becf2d8a41f6d1ee100c76daa2466db22
Davinaputku <davinaputri2408@gmail.com>
2024-10-27 16:18:31 +0700

    struktur folder

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit explicabo autem corporis deserunt laudantium nobis repellat minus ducimus odit neque eligendi saepe optio esse modi asperiores suscipit, iure officiis eius tenetur molestiae facere. Veniam iste esse eos qui porro, culpa veritatis nihil, enim dicta dignissimos nobis facere voluptatibus consectetur! Nam!</p>
+    </article>
+
+    <article>
+        <h3>Judul 2</h3>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quasi itaque nobis provident laboriosam nulla mollitia sunt, tempora magni pariatur accusantium illo deleniti, aliquid dignissimos laborum modi voluptate dolorem iusto assumenda esse blanditiis! Quisquam voluptate fugit numquam natus inventore consectetur?
+    </article>
+</body>
+</html>
