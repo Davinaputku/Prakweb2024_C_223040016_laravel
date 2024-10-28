e85ec38237f92c5cc785260974d7d989e23493d5
Davinaputku <davinaputri2408@gmail.com>
2024-10-28 22:56:41 +0700

    #14 N+1 Problem

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    // melakukan query dengan eager loading
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{pots:slug}', function (Post $post) {
@@ -22,15 +25,18 @@ Route::get('/posts/{pots:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
-Route::get('/authors/{user:username}', function(User $user) {
+Route::get('/authors/{user:username}', function (User $user) {
+    // melakukan eager loading pada author dan category pada post yang dimiliki oleh user
+    // $posts = $user->posts->load('category', 'author');
     return view('posts', [
         'title' => count($user->posts) . ' Articles By ' . $user->name,
         'posts' => $user->posts
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view ('posts', ['title' => 'Article in :' . $category->name, 'posts' => $category->posts]);
+Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load('category', 'author');
+    return view('posts', ['title' => 'Article in :' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
