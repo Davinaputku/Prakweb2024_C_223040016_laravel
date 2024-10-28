276d2a7e2a2196e040abdc360aeb85cdc732ae1c
Davinaputku <davinaputri2408@gmail.com>
2024-10-28 20:16:07 +0700

    #5 Blade Component

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Davina Putri Kusuma']);
+    return view('about', ['name' => 'Davina Putri Kusuma', 'title' => 'About']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
+
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
